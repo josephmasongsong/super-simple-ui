df67d08197757539477f942dfbd5e50dcc90d645
Joseph Masongsong <joseph@josephmasongsong.com>
2022-04-18 21:28:10 -0700

    added prettier config and copy webpack plugin instead of copyfiles

--- example/prettier.config.js
+++ example/prettier.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  arrowParens: 'avoid',
+  semi: true,
 };
--- example/webpack.prod.js
+++ example/webpack.prod.js
@@ -2,10 +2,16 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '**/*.html' }],
+    }),
+  ],
 });
